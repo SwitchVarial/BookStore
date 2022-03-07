68115c0e8fc0fcfa04b8ff3f65817be610baef41
SwitchVarial <kimmo.lindeman@gmail.com>
2022-03-07 22:06:37 +0200

    REST omaksi controlleriksi

--- src/main/java/project/bookstore/web/BookController.java
+++ src/main/java/project/bookstore/web/BookController.java
@@ -1,15 +1,11 @@
 package project.bookstore.web;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import project.bookstore.domain.Book;
 import project.bookstore.domain.BookRepository;
@@ -58,19 +54,6 @@ public class BookController {
 		model.addAttribute("book", repository.findById(id));
 		model.addAttribute("categories", crepository.findAll());
 		return "editbook";
-	}
-	
-	//REST kaikki kirjat
-	@RequestMapping(value="/booklist", method = RequestMethod.GET)
-	public @ResponseBody List<Book> booklistRest() {
-		return (List<Book>) repository.findAll();
-	}
-	
-	//Rest hakee kirjan id:n perusteella
-	@RequestMapping(value = "/booklist/{id}", method = RequestMethod.GET)
-	public @ResponseBody Optional<Book> findbookRest(@PathVariable("id") Long studentId) {
-		return repository.findById(studentId);
-	}
-	
+	}	
 
 }
