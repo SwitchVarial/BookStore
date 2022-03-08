7bd72503ef9c892b051ce46ba30d7954db64ac2a
SwitchVarial <kimmo.lindeman@gmail.com>
2022-03-08 19:44:48 +0200

    JUnit - Smoke Testing

--- src/test/java/project/bookstore/BookstoreApplicationTests.java
+++ src/test/java/project/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,34 @@
 package project.bookstore;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import project.bookstore.web.BookController;
+import project.bookstore.web.BookRestController;
 
 @SpringBootTest
 class BookstoreApplicationTests {
+	
+	@Autowired
+	private BookController bookController;
 
 	@Test
-	void contextLoads() {
+	@DisplayName("BookController should Not be null")
+	void contextLoadsBook() {
+		assertThat(bookController, notNullValue());
 	}
-
+	
+	@Autowired
+	private BookRestController bookRestController;
+	
+	@Test
+	@DisplayName("BookRestController should not be null")
+	void contextLoadsBookRest() {
+		assertThat(bookRestController, notNullValue());
+	}
 }
